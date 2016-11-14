23e6619558188b6bb3aaa644f49d2b6d7de2b6a5
Hector Mendoza <hmendoza@24hourfit.com>
2016-11-14 14:09:26 -0800

    add path to git execution

--- git.go
+++ git.go
@@ -7,15 +7,8 @@ import (
 )
 
 func executeGitAction(rootPath, action string) (status int, err error) {
-	log.Println("Path to run Git on: " + rootPath)
-	cmd := exec.Command("git", action)
+	log.Println("Path to run git on: " + rootPath)
+	cmd := exec.Command("git", "-C", rootPath, action)
 	cmd.Env = os.Environ()
-	/*
-		for _, element := range cmd.Env {
-			if strings.LastIndex(element, "GIT_EXEC_PATH") >= 0 {
-				fmt.Println(element)
-			}
-		}
-	*/
 	return executeCommand(cmd)
 }
